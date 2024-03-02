140a318887c94397d131dd00c085b9397a202289
Xavier Chen <xavier.chen2000@gmail.com>
2024-03-02 16:58:42 +0000

    Updated subscriber script with better documentations and defaults.

--- src/flower_subscriber/flower_subscriber/fl_subscriber.py
+++ src/flower_subscriber/flower_subscriber/fl_subscriber.py
@@ -1,7 +1,7 @@
 # import rclpy
 from rclpy.node import Node
 from ml_interfaces_py.lib import FloatTensor, FeatureLabelPair
-from typing import Callable, List, Tuple
+from typing import Callable, List, Tuple, Optional
 
 import torch
 from torch import Tensor
@@ -9,20 +9,29 @@ from os import path
 from datatime import datetime
 
 
+def _raise(ex: Exception):
+    """Wrapper function to raise exceptions in lambda"""
+    raise ex
+
+
 class FlowerSubscriber(Node):
     """
-    ROS node responsible for feeding data to flower client, in which the training occurs
+    ROS node responsible for feeding data sets to flower client,
+    communicates with the flower client via a hook function
     """
 
-    flower_hook: Callable
+    flower_hook_t = Callable[str]
+    flower_hook: flower_hook_t
     feature_label_pairs: List[Tuple[Tensor, Tensor]] = []
     package_size: int = 0
-    data_package_limit: int
+    data_package_limit: int = 100  # 100 training samples per batch
     data_folder: str = path.join(path.dirname(path.abspath(__file__)), "data")
 
     def __init__(
         self,
-        flower_hook_fn: Callable,
+        flower_hook_fn: Optional[Callable] = lambda: _raise(
+            NotImplementedError
+        ),  # Default to raise NotImplemented
         msg_type=FeatureLabelPair,
         data_package_limit: int = 100,
     ):
@@ -36,6 +45,12 @@ class FlowerSubscriber(Node):
         self.external_callback = flower_hook_fn
         self.data_package_limit = data_package_limit
 
+    def set_flower_hook(self, flower_hook_fn: flower_hook_t):
+        """
+        Changes flower hook function
+        """
+        self.flower_hook = flower_hook_fn
+
     def listener_callback(self, msg: FeatureLabelPair):
         """
         Callback when subscriber receives broadcast from publisher
@@ -44,7 +59,9 @@ class FlowerSubscriber(Node):
         """
         # TODO: link this to be handled in the flower client
         if self.package_size >= self.data_package_limit:
-            save_path = path.join(self.data_folder, str(datetime.now().timestamp()))
+            save_path = path.join(
+                self.data_folder, str(datetime.now().timestamp(), ".pb")
+            )
             torch.save(
                 self.feature_label_pairs,
                 f"{save_path}",
@@ -52,6 +69,12 @@ class FlowerSubscriber(Node):
             # For message passing between ROS node that flower client;
             # currently it only sends the processed training data package
             # oo the flower client
-            self.flower_hook(save_path)
+            try:
+                self.flower_hook(save_path)
+            except NotImplementedError:
+                raise NotImplementedError(
+                    "Flower hook is required for communication with flower"
+                )
+
             self.feature_label_pairs.clear()
         self.feature_label_pairs.append(msg.torch())
--- src/flower_subscriber/flower_subscriber/flwr_client.py
+++ src/flower_subscriber/flower_subscriber/flwr_client.py
@@ -1,1 +1,15 @@
 from fl_subscriber import FlowerSubscriber
+from flwr import Client
+from typing import List, Tuple
+
+
+class RosClient(Client):
+    training_data_dir: List[str] = []
+    subscriber_node: FlowerSubscriber
+
+    def __init__(self, subscriber_node: FlowerSubscriber):
+        super.__init__(self)
+        self.subscriber_node = subscriber_node
+        self.subscriber_node.set_flower_hook(
+            lambda self, training_data: self.training_data_dir.append(training_data)
+        )
