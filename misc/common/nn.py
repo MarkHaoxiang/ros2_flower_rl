85441958dc2693ad8aeace43fc8655f4e9f7baab
MarkHaoxiang <mark.haoxiang@gmail.com>
2024-03-04 18:11:19 +0000

    Toy Fl server (second try)

---
+++ misc/common/nn.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+
+class MnistClassifier(nn.Module):
+    """ This class defines a basic convolutional neural nework for classifing the MNIST dataset of digits
+    """
+    def __init__(self):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+        nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, padding=3),
+        nn.LeakyReLU(),
+        nn.MaxPool2d(kernel_size=3),
+        nn.Conv2d(in_channels=16,out_channels=32, kernel_size=3, padding=3),
+        nn.LeakyReLU(),
+        nn.Conv2d(in_channels=32,out_channels=64, kernel_size=3, padding=3),
+        nn.LeakyReLU()
+        )
+        self.flatten = nn.Flatten(start_dim=-3, end_dim=-1)
+        self.mlp = nn.Sequential(
+            nn.LazyLinear(out_features=32),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=32, out_features=10)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = (x / 256).unsqueeze(-3)
+        x = x.to(torch.float32)
+        x = self.conv(x)
+        x = self.flatten(x)
+        x = self.mlp(x)
+        return x
---
+++ misc/toy_fl_server.py
@@ -0,0 +1,58 @@
+from datasets import load_dataset
+import torch
+from torch.utils.data import DataLoader
+import flwr as fl
+from flwr.common import NDArrays
+from florl.common.util import aggregate_weighted_average, set_torch_parameters
+
+from common.nn import MnistClassifier
+
+DATASET = "mnist"
+
+def _on_fit_config_fn(server_round: int):
+    return {"server_round": server_round}
+def _on_evaluate_config_fn(server_round: int):
+    return {"server_round": server_round}
+
+def main():
+    # Load Data
+    dataset = load_dataset(DATASET).with_format("torch")["test"]
+    dataloader = DataLoader(dataset, batch_size=32)
+    cross_entropy_loss = torch.nn.CrossEntropyLoss()
+
+    net = MnistClassifier()
+    def evaluate(server_round: int, parameters: NDArrays, config):
+        set_torch_parameters(net, parameters)
+        # Run evaluation round
+        average_loss, correct = 0, 0
+        size, num_batches = len(dataset), len(dataloader)
+        for batch in dataloader:
+            X, y = batch["image"], batch["label"]
+            # Predict Labels
+            py = net(X)
+            # Loss
+            average_loss += cross_entropy_loss(py, y).item()
+            # Correct
+            correct += (py.argmax(1) == y).type(torch.float).sum().item()
+        average_loss = average_loss / num_batches
+        accuracy = correct / size
+        # Return results
+        return average_loss, {"accuracy": accuracy}
+
+    strategy = fl.server.strategy.FedAvg(
+        on_fit_config_fn = _on_fit_config_fn,
+        on_evaluate_config_fn= _on_evaluate_config_fn,
+        fit_metrics_aggregation_fn=aggregate_weighted_average,
+        evaluate_metrics_aggregation_fn=aggregate_weighted_average, 
+        evaluate_fn=evaluate,
+        accept_failures=False
+    )
+
+    fl.server.start_server(
+        server="0.0.0.0:8080",
+        config=fl.server.ServerConfig(num_rounds=10),
+        strategy=strategy
+    )
+
+if __name__ == "__main__":
+    main()
