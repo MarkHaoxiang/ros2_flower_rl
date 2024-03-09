d711e7812cbc0869a83822dd77278bf2cdad1ebb
MarkHaoxiang <mark.haoxiang@gmail.com>
2024-03-09 11:26:15 +0000

    Parametrized buffer type

--- src/replay_buffer/src/replay_buffer.cpp
+++ src/replay_buffer/src/replay_buffer.cpp
@@ -2,24 +2,22 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-
-// #include "ml_interfaces/msg/FloatTensor.hpp"
-
 using std::placeholders::_1;
 
+template <typename T>
 class ReplayBuffer : public rclcpp::Node
 {
 public:
     ReplayBuffer() : Node("minimal_subscriber")
     {
-        subscription_ = this->create_subscription<std_msgs::msg::String>(
+        subscription_ = this->create_subscription<T>(
             "topic",
             10,
             std::bind(&ReplayBuffer::topic_callback, this, _1));
     }
 
 private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    void topic_callback(const typename T::SharedPtr msg) const
     {
         RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
     }
@@ -29,7 +27,8 @@ private:
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ReplayBuffer>());
+    auto node = std::make_shared<ReplayBuffer<std_msgs::msg::String>>();
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
